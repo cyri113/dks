5d4548a55162331b3154f3c68cebe9be2386349d
Cyrille Derche <c.derche@me.com>
2014-06-25 00:02:11 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dokspot::Application.config.secret_key_base = '1995f87aea17719a3f16785f118a643f58d67dfcd5b779f0b34bbc492b9bb31643ab6c36097489056ab9df323730fec25f87b8b80eba5c26c281f9f46ca989ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dokspot::Application.config.session_store :cookie_store, key: '_dokspot_session'
